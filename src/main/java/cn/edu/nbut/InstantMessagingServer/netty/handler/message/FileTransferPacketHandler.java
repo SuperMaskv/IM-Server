1c63cf22bf75f65ed5315edeffad8a862001ee23
SuperMaskv <supermaskv@foxmail.com>
2020-03-10 16:23:59 +0800

    Rebuild FileTransferPacketHandler from service layer.

--- src/main/java/cn/edu/nbut/InstantMessagingServer/netty/handler/message/FileTransferPacketHandler.java
+++ src/main/java/cn/edu/nbut/InstantMessagingServer/netty/handler/message/FileTransferPacketHandler.java
@@ -1,14 +1,14 @@
 package cn.edu.nbut.InstantMessagingServer.netty.handler.message;
 
 
-import cn.edu.nbut.InstantMessagingServer.connection.ConnectionMap;
 import cn.edu.nbut.InstantMessagingServer.protocol.packet.message.FileTransferPacket;
+import cn.edu.nbut.InstantMessagingServer.service.ContactService;
+import cn.edu.nbut.InstantMessagingServer.service.UserService;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 
 import java.net.InetSocketAddress;
 
@@ -20,17 +20,24 @@ import java.net.InetSocketAddress;
 @Component
 @ChannelHandler.Sharable
 public class FileTransferPacketHandler extends SimpleChannelInboundHandler<FileTransferPacket> {
+    private UserService userService;
+    private ContactService contactService;
+
     @Autowired
-    private ConnectionMap connectionMap;
+    public FileTransferPacketHandler(UserService userService, ContactService contactService) {
+        this.userService = userService;
+        this.contactService = contactService;
+    }
+
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext
             , FileTransferPacket fileTransferPacket) throws Exception {
-        if (!connectionMap.isTokenExist(fileTransferPacket.getToken())) return;
-        if (!connectionMap.isUserExist(fileTransferPacket.getFileReceiver())) return;
-        if (!connectionMap.isUserExist(fileTransferPacket.getFileSender())) return;
+        if (!userService.validateToken(fileTransferPacket.getToken())) return;
+        if (!userService.isUserLogged(fileTransferPacket.getFileReceiver())) return;
+        if (!userService.isUserLogged(fileTransferPacket.getFileSender())) return;
 
-        var receiverChannel = connectionMap
-                .getChannelByUserName(fileTransferPacket.getFileReceiver());
+        var receiverChannel = contactService
+                .getLoggedContactChannel(fileTransferPacket.getFileReceiver());
 
         if (fileTransferPacket.isRequestFlag()) {
             //请求报文:将请求报文转发给接收用户
@@ -38,8 +45,8 @@ public class FileTransferPacketHandler extends SimpleChannelInboundHandler<FileT
         } else {
             //应答报文：根据agreeFlag来分别处理
 
-            var senderChannel = connectionMap
-                    .getChannelByUserName(fileTransferPacket.getFileSender());
+            var senderChannel = contactService
+                    .getLoggedContactChannel(fileTransferPacket.getFileSender());
 
             if (fileTransferPacket.isAgreeFlag()) {
                 //如果接收方同意将接收文件
